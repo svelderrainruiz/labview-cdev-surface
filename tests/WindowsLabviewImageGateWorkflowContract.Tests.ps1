33fcaf89d7ff76978680983b97c5ea68bd68f1fd
Sergio Velderrain <svelderrain_ruiz@hotmail.com>
2026-02-23 17:16:47 -0800

    fix(gate): correct nipkg feed syntax and sync cdev-cli v0.1.0

--- tests/WindowsLabviewImageGateWorkflowContract.Tests.ps1
+++ tests/WindowsLabviewImageGateWorkflowContract.Tests.ps1
@@ -48,6 +48,8 @@ Describe 'Windows LabVIEW image gate workflow contract' {
         $script:coreWorkflowContent | Should -Match '--env "LVIE_GATE_REQUIRED_LABVIEW_YEAR=\$gateRequiredLabviewYear"'
         $script:coreWorkflowContent | Should -Match '--env "LVIE_GATE_SINGLE_PPL_BITNESS=\$singlePplBitness"'
         $script:coreWorkflowContent | Should -Match '--env "LVIE_LABVIEW_X86_NIPKG_INSTALL_CMD=\$labviewX86NipkgInstallCmd"'
+        $script:coreWorkflowContent | Should -Match 'feed-add https://download\.ni\.com/support/nipkg/products/ni-l/ni-labview-2026-x86/26\.1/released --name ni-labview-2026-core-x86-en-2026-q1-released'
+        $script:coreWorkflowContent | Should -Match 'feed-add https://download\.ni\.com/support/nipkg/products/ni-l/ni-labview-2020-x86/20\.0/released --name ni-labview-2020-core-x86-en-2020-released'
         $script:coreWorkflowContent | Should -Match '--mount "type=bind,source=\$hostPwshRoot,target=C:\\host-tools\\PowerShell7,readonly"'
         $script:coreWorkflowContent | Should -Match '--mount "type=bind,source=\$hostGitRoot,target=C:\\host-tools\\Git,readonly"'
         $script:coreWorkflowContent | Should -Match '--mount "type=bind,source=\$hostGhRoot,target=C:\\host-tools\\GitHubCLI,readonly"'
